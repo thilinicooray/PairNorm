06f1e9e755233f8ee30b193598b17bbfb088da33
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-25 09:57:00 +0800

    add input

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 from layers import *
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class SGC(nn.Module):
     # for SGC we use data without normalization
     def __init__(self, nfeat, nhid, nclass, dropout, nlayer=2, norm_mode='None', norm_scale=10, **kwargs):
@@ -25,18 +29,57 @@ class GCN(nn.Module):
         self.gc1 = GraphConv(nfeat, nhid)
         self.gc2 = GraphConv(nhid, nclass)
 
+        #vae
+        self.gc_mu_adj = GraphConv(nhid, nhid)
+        self.gc_logvar_adj = GraphConv(nhid, nhid)
+
+        self.gc_mu_node = GraphConv(nhid, nhid)
+        self.gc_logvar_node = GraphConv(nhid, nhid)
+
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+        self.node_regen = GraphConv(nhid, nhid+nfeat)
+
         self.dropout = nn.Dropout(p=dropout)
         self.relu = nn.ReLU(True)
         self.norm = PairNorm(norm_mode, norm_scale)
 
-    def forward(self, x, adj):
-        x = self.dropout(x)
-        x = self.gc1(x, adj)
+    def encode(self, hidden1, adj, gc2, gc3, gc4, gc5):
+        return self.relu(gc2(hidden1, adj)), self.relu(gc3(hidden1, adj)), self.relu(gc4(hidden1, adj)), self.relu(gc5(hidden1, adj))
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def forward(self, x_org, adj):
+        x = self.dropout(x_org)
+        x = self.relu(self.gc1(x, adj))
+
+        #vae
+        mu, logvar, mu_n, var_n = self.encode(self.dropout(x), adj, self.gc_mu_adj, self.gc_logvar_adj, self.gc_mu_node, self.gc_logvar_node)
+        z = self.reparameterize(mu, logvar)
+        z_n = self.reparameterize(mu_n, var_n)
+        adj1 = self.dc(z)
+
+
+        #get masked new adj
+        zero_vec = -9e15*torch.ones_like(adj1)
+        masked_adj = torch.where(adj > 0, adj1, zero_vec)
+        adj1 = F.softmax(masked_adj, dim=1)
+
+        a1 = self.relu(self.node_regen(self.dropout(z_n), adj1.t()))
+        zero_vec = -9e15*torch.ones_like(a1)
+        masked_nodes = torch.where(x_org > 0, a1, zero_vec)
+        a1 = F.softmax(masked_nodes, dim=1)
+
+        x = torch.cat([a1, x], -1)
         #x = self.norm(x)
-        x = self.relu(x)
         x = self.dropout(x)
-        x = self.gc2(x, adj)
-        return x
+        x = self.gc2(x, adj+adj1)
+        return a1, adj1 , mu, logvar, mu_n, var_n, x
 
 class GAT(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout, nhead, 
@@ -122,3 +165,17 @@ class DeepGAT(nn.Module):
         x = self.dropout(x)
         x = self.out_layer(x, adj)
         return x
+
+
+class InnerProductDecoder(nn.Module):
+    """Decoder for using inner product for prediction."""
+
+    def __init__(self, dropout, act=torch.sigmoid):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.act = act
+
+    def forward(self, z):
+        z = F.dropout(z, self.dropout, training=self.training)
+        adj = self.act(torch.mm(z, z.t()))
+        return adj
--- utils.py
+++ utils.py
@@ -1,8 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def loss_function(preds, labels, mu, logvar, n_nodes):
+    cost = F.binary_cross_entropy_with_logits(preds, labels)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 / n_nodes * torch.mean(torch.sum(
+        1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))
+    return cost + KLD
+
 def train(net, optimizer, criterion, data):
     net.train()
     optimizer.zero_grad()
-    output = net(data.x, data.adj)
-    loss = criterion(output[data.train_mask], data.y[data.train_mask])
+    noderegen, recovered, mu, logvar, mu_n, var_n, output = net(data.x, data.adj)
+    nc_loss = criterion(output[data.train_mask], data.y[data.train_mask])
+    ae_loss = loss_function(preds=recovered[data.train_mask], labels=data.adj[data.train_mask],
+                            mu=mu[data.train_mask], logvar=logvar[data.train_mask], n_nodes=data.adj.size[0])
+    node_ae_loss = loss_function(preds=noderegen[data.train_mask], labels=data.x[data.train_mask],
+                                 mu=mu_n[data.train_mask], logvar=var_n[data.train_mask], n_nodes=data.adj.size[0])
+    loss = nc_loss + 0.1*ae_loss + 0.1*node_ae_loss
     acc = accuracy(output[data.train_mask], data.y[data.train_mask])
     loss.backward()
     optimizer.step()
